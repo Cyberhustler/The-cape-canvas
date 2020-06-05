425204d17d4806ba62ac18fd256bd3b6c02e4cfa
Cyberhustler <62091903+Cyberhustler@users.noreply.github.com>
2020-06-06 01:39:29 +0200

    the wave and control are all calculated and the
    Ask if confused?

---
+++ cape.js
@@ -0,0 +1,50 @@
+
+var c = document.createElement("canvas");
+var ctx = c.getContext("2d");
+c.width=500;
+c.height=350;
+document.body.appendChild(c);
+
+var perm = [];
+while (perm.length < 255){
+	while(perm.includes(val = Math.floor(Math.random()*255)));
+		  perm.push(val);
+}
+
+var lerp = (a,b,t) => a + (b-a) * (1-Math.cos(t*Math.PI))/2;
+var noise = x=>{
+	x = x * 0.01  % 255;
+	return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
+}
+
+var player = new function(){
+	this.x = c.width/2;
+	this.y = 0;
+	this.rot = 0;
+	
+	this.img = new Image();
+	this.img.src = "https://vignette.wikia.nocookie.net/vsbattles/images/f/f5/Emmet.png/revision/latest?cb=20150411114241";
+	this.draw = function(){
+		ctx.drawImage(this.img,this.x, 100);
+	}
+}
+
+var t = 0;
+function loop(){ 
+	t+=1;
+	ctx.fillStyle = "#19f";
+	ctx.fillRect(0,0,c.width,c.width);
+	
+	ctx.fillStyle = "black";
+	ctx.beginPath();
+	ctx.moveTo(0,c.height);
+	for(let i = 0; i < c.width; i++)
+		ctx.lineTo(i, c.height - noise(i + t) * 0.25);
+	
+	ctx.lineTo(c.width,c.height);
+	ctx.fill();
+	player.draw();
+	requestAnimationFrame(loop);
+}
+
+loop();
